ccd87655403dab69e7872d30ee90d1a0a5e570aa
xiowei <xiongwei@git.com>
2018-07-21 21:32:30 +0800

    2018年7月21日

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'nihao' });
+});
+router.get('/about', function(req, res, next) {
+    res.send('about..');
+});
+
+module.exports = router;
